456929a69db355bca6b395ef057079cafb4cc0a2
Erland Lie Amundgård <erlandla@stud.ntnu.no>
2021-12-20 01:14:53 +0100

    Add start of ui

---
+++ src/main/java/cardgames/App.java
@@ -0,0 +1,24 @@
+package cardgames;
+
+import java.io.IOException;
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+
+public class App extends Application {
+
+    @Override
+    public void start(Stage stage) throws IOException {
+        FXMLLoader fxmlLoader = new FXMLLoader(this.getClass().getResource("fxml/solitaire.fxml"));
+        stage.setTitle("Solitaire");
+        Parent parent = fxmlLoader.load();
+        stage.setScene(new Scene(parent));
+        stage.show();
+    }
+
+    public static void main(String[] args) {
+        launch();
+    }
+}
---
+++ src/main/java/cardgames/SolitaireController.java
@@ -0,0 +1,50 @@
+package cardgames;
+
+import javafx.fxml.FXML;
+import javafx.scene.layout.Pane;
+
+public class SolitaireController {
+
+    @FXML
+    private Pane tableau1;
+
+    @FXML
+    private Pane tableau2;
+
+    @FXML
+    private Pane tableau3;
+
+    @FXML
+    private Pane tableau4;
+
+    @FXML
+    private Pane tableau5;
+
+    @FXML
+    private Pane tableau6;
+
+    @FXML
+    private Pane tableau7;
+
+    @FXML
+    private Pane handPile;
+
+    @FXML
+    private Pane wastePile;
+
+    @FXML
+    private Pane suitPile1;
+
+    @FXML
+    private Pane suitPile2;
+
+    @FXML
+    private Pane suitPile3;
+
+    @FXML
+    private Pane suitPile4;
+
+    void initialize() {
+
+    }
+}
